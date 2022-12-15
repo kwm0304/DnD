002b085a02ac247261a8047b8d418b45b290bb1f
Kenan <mckenzie.kenan@yahoo.com>
2022-12-14 20:41:04 -0500

    round2

--- server/models/Character.js
+++ server/models/Character.js
@@ -1,1 +1,56 @@
 const {Schema, model } = require('mongoose')
+
+let CharacterSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    race: {
+        type: String,
+        allowNull: false
+    },
+    stats: {
+        strength: {
+            type: Number,
+            required: true
+        },
+        dexterity: {
+            type: Number,
+            required: true,
+        },
+        constitution: {
+            type: Number,
+            required: true
+        },
+        intellegence: {
+            type: Number,
+            required: true
+        },
+        wisdom: {
+            type: Number,
+            required: true
+        },
+        charisma: {
+            type: Number,
+            required: true
+        }
+},
+spells: [{
+    type: String,
+    required: false
+}],
+proficiencyBonus: {
+    type: Number,
+    required: false
+},
+passivePerception: {
+    type: Number,
+    required: false
+},
+weapons: {
+    type: String,
+    required: true
+}
+
+})
