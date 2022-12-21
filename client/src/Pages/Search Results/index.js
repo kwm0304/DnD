d44d0d88680167f1ce37359eb87f7d8598a3d638
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-20 21:37:09 -0500

    updated search results route to return character card

--- client/src/Pages/Search Results/index.js
+++ client/src/Pages/Search Results/index.js
@@ -1,20 +1,6 @@
 // Search Result componenet
-import React, { useEffect, useState } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
+import React from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 
-export const SearchResults = () => {
-  const [searchResults, setSearchResults] = useState([]);
-  const [error, setError] = useState(false);
-  const location = useLocation();
-  const query = new URLSearchParams(location.search).get("query");
-  const { slug } = useParams();
-  useEffect(() => {
-    const searchProduct = async () => {
-      try {
-        const { data } = await axios.get(`API_URL/search?q=${query}`); setSearchResults(data.products);
-        } catch (error) {
-         setError(error.response?.data?.message);
-         }
-        };
-         searchProduct();
-       }, []);
+const SearchChar = ({ character }) => {
+    console.log(!character.name);
