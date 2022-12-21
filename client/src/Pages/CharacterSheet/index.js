b5472cfadc3a016bb79b4ce0bd164b59c3362f31
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2022-12-21 13:18:48 -0500

    finished charactersheet

--- client/src/Pages/CharacterSheet/index.js
+++ client/src/Pages/CharacterSheet/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function CharacterSheet() {
   return (
@@ -28,27 +28,39 @@ function CharacterSheet() {
         <Form.Control 
         type="number"  name='strength' value={formState.name} onChange={handleChange}/>
         <Form.Control 
-        type="number" placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
+        type="number"  name='dexterity' value={formState.dexterity} onChange={handleChange}/>
         <Form.Control 
-        type="number" placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
+        type="number"  name='constitution' value={formState.constitution} onChange={handleChange}/>
         <Form.Control 
-        type="number" placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
+        type="number"  name='intelligence' value={formState.intelligence} onChange={handleChange}/>
         <Form.Control 
-        type="number" placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
+        type="number"  name='wisdom' value={formState.wisdom} onChange={handleChange}/>
         <Form.Control 
-        type="number" placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
+        type="number"  name='charisma' value={formState.charisma} onChange={handleChange}/>
         <Form.Control 
-        type="number" placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
+        type="number"  name='proficiencyBonus' value={formState.proficiencyBonus} onChange={handleChange}/>
         <Form.Control 
-        type="number" placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
-        <Form.Control 
-        type="n
-        " placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
-        <Form.Control 
-        type="text" placeholder="Hingle McCringleberry" name='name' value={formState.name} onChange={handleChange}/>
+        type="number"  name='passivePerception' value={formState.passivePerception} onChange={handleChange}/>
       </Form.Group>
 
-      
+      <Form.Group className='mb-3' controlId='spells'>
+        <Form.Label>Spells</Form.Label>
+        <Form.Control
+        type='text' name='spells' value={formState.spells} onChange={handleChange} />
+      </Form.Group>
+
+      <Form.Group className='mb-3' controlId='weapons'>
+        <Form.Label>Weapon</Form.Label>
+        <Form.Control
+        type='text' name='weapons' value={formState.weapons} onChange={handleChange} />
+      </Form.Group>
+
+      <Form.Group className='mb-3' controlId='background'>
+        <Form.Label>Background</Form.Label>
+        <Form.Control
+        type='text' name='background' value={formState.background} onChange={handleChange} />
+      </Form.Group>
+
     </Form>
   );
 }
